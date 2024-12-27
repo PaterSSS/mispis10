04c1dd04b37face75bfaf472c060074ddaa34bc5
Leonid <leo.chel@mail.ru>
2024-12-27 03:06:07 +0300

    first commit

---
+++ src/main/java/org/leochel/Participation.java
@@ -0,0 +1,19 @@
+package org.leochel;
+
+public class Participation {
+
+	private int hours;
+
+	public Participation(int hours) {
+		this.hours = hours;
+	}
+
+	public int getHours() {
+		return this.hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+}
